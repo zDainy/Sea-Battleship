40fd44eeb44d288371ef801aeb6ee0cdff123182
Alex Faint <pinky.pony@outlook.com>
2017-09-17 21:35:13 +0400

    Revert "Revert "Запись в лог и открытие сокета сервера""
    This reverts commit b280cdb132c34e13401d10f6ce418f0b4d3c16ff.

---
+++ Network/ServerUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using Common;
+
+namespace Network
+{
+    public static class ServerUtils
+    {
+        private static Stream _stream;
+        private static IPAddress _ip;
+        private static StreamReader _sr;
+
+        /// <summary>
+        /// Получает внешний IP-адрес
+        /// </summary>
+        public static IPAddress GetIP()
+        {
+            try
+            {
+                LogService.Trace("Пытаемся получить внешний IP-адрес с сайта");
+                WebClient wClient = new WebClient();
+                _stream = wClient.OpenRead("http://www.ip-ping.ru/");
+                _sr = new StreamReader(_stream);
+                string newLine;
+                Regex regex = new Regex("<div class=\"hc2\">(.*)</div>");
+                while ((newLine = _sr.ReadLine()) != null)
+                {
+                    Match match = regex.Match(newLine);
+                    string str = match.Groups[1].ToString();
+                    if (str != "")
+                    {
+                        _ip = IPAddress.Parse(str);
+                        LogService.Trace($"IP-адрес получен: {str}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogService.Trace($"Ошибка получения IP-адреса: {e}");
+            }
+            finally
+            {
+                _sr.Close();
+                _stream.Close();
+            }
+            return _ip;
+        }
+    }
+}
---
+++ Network/SocketServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Common;
+
+namespace Network
+{
+
+    public class SocketServer
+    {
+        // Устанавливаем порт для TcpListener
+        private int _port = 27015;
+        private TcpListener _server;
+        private TcpClient _client;
+        private IPAddress _ipAddress;
+
+        /// <summary>
+        /// Открывает сокет сервера
+        /// </summary>
+        public void Open(IPAddress ip)
+        {
+            try
+            {
+                LogService.Trace("Открываем сокет");
+                _ipAddress = ip;
+                _server = new TcpListener(_ipAddress, _port);
+
+                // Запускаем TcpListener и начинаем слушать клиентов.
+                _server.Start();
+                LogService.Trace("Ожидание подключений...");
+                while (true)
+                {
+                    // Ожидаем входящее соединение
+                    _client = _server.AcceptTcpClient();
+                    LogService.Trace("Клиент подключен");
+                }
+            }
+            catch (SocketException e)
+            {
+                LogService.Trace($"Не удалось создать сервер: {e}");
+            }
+        }
+
+        public void Stop()
+        {
+            _server.Stop();
+        }
+    }
+}
