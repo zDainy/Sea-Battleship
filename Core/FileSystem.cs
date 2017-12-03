c32bd4d06d2b65972c30e753458c80d08a06ae66
noSerious217 <gidrolisk314@yandex.ru>
2017-12-03 14:54:42 +0400

    Переработаны функции сохранения и загрузки расстановки для работы с новым классом

--- Core/FileSystem.cs
+++ Core/FileSystem.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
+    /// <summary>
+    /// Подсистема работы с файлами игры.
+    /// </summary>
     class FileSystem
     {
         private static Random random = new Random();
@@ -83,118 +84,43 @@ namespace ConsoleApp3
             return result;
         }
 
-        private static byte GetHash(byte[] input, int type)
+        private static byte[] GetHash(byte[] input)
         {
-            if (input.Length == 2)
+            int size = (int)Math.Round(Math.Sqrt(input.Length));
+            List<byte>[] hash = new List<byte>[size];
+            for (int i = 0; i < size; i++)
             {
-                switch (type)
-                {
-                    case (1):
-                        return (byte)(input[0] & input[1]);
-                    case (2):
-                        return (byte)(input[0] | input[1]);
-                    case (3):
-                        return (byte)(input[0] ^ input[1]);
-                    case (4):
-                        return (byte)(input[0] + (input[1] >> 1));
-                }
+                hash[i] = new List<byte>();
+                hash[i].Add((byte)(input.Length + i));
             }
-            if (input.Length == 3)
+            byte[] res = new byte[size];
+            for (int i = 0; i < input.Length; i++)
             {
-                switch (type)
-                {
-                    case (1):
-                        return (byte)(input[0] & input[1] ^ input[2]);
-                    case (2):
-                        return (byte)(input[0] | input[1] ^ input[2]);
-                    case (3):
-                        return (byte)(input[0] ^ input[1] & input[2]);
-                    case (4):
-                        return (byte)(input[0] ^ input[1] | input[2]);
-                    case (5):
-                        return (byte)(input[0] & input[1] | input[2]);
-                    case (6):
-                        return (byte)(input[0] ^ input[1] ^ input[2]);
-                }
+                int p;
+                Math.DivRem(i, size, out p);
+                hash[p].Add(input[i]);
             }
-            if (input.Length == 5)
+            for (int i = 0; i < size; i++)
             {
-                byte[] first = new byte[3] { input[0], input[1], input[2] };
-                byte[] second = new byte[3] { 0, input[3], input[4] };
-                switch (type)
+                byte b = (byte)hash[i].Count;
+                foreach (byte z in hash[i])
                 {
-                    case (1):
-                        second[0] = GetHash(first, 1);
-                        return GetHash(second, 3);
-                    case (2):
-                        second[0] = GetHash(first, 2);
-                        return GetHash(second, 6);
-                    case (3):
-                        second[0] = GetHash(first, 2);
-                        return GetHash(second, 4);
-                    case (4):
-                        second[0] = GetHash(first, 1);
-                        return GetHash(second, 1);
-                    case (5):
-                        second[0] = GetHash(first, 4);
-                        return GetHash(second, 4);
-                    case (6):
-                        second[0] = GetHash(first, 5);
-                        return GetHash(second, 2);
-                    case (7):
-                        second[0] = GetHash(first, 3);
-                        return GetHash(second, 4);
-                    case (8):
-                        second[0] = GetHash(first, 3);
-                        return GetHash(second, 5);
-                    case (9):
-                        second[0] = GetHash(first, 1);
-                        return GetHash(second, 5);
-                    case (10):
-                        second[0] = GetHash(first, 6);
-                        return GetHash(second, 3);
+                    b = (byte)(b ^ z);
                 }
+                res[i] = b;
             }
-            if (input.Length == 8)
+            return res;
+        }
+
+        private static bool CheckHash(byte[] input, byte[] hash)
+        {
+            byte[] gethash = GetHash(input);
+            if (gethash.Length != hash.Length) return false;
+            for (int i = 0; i < hash.Length; i++)
             {
-                byte[] first = new byte[3] { input[0], input[1], input[2] };
-                byte[] second = new byte[3] { input[3], input[4], input[5] };
-                byte[] third = new byte[2] { input[6], input[7] };
-                switch (type)
-                {
-                    case (1):
-                        byte[] res1 = new byte[3] { GetHash(first, 1), GetHash(second, 3), GetHash(third, 2) };
-                        return GetHash(res1, 4);
-                    case (2):
-                        byte[] res2 = new byte[3] { GetHash(first, 2), GetHash(second, 6), GetHash(third, 3) };
-                        return GetHash(res2, 5);
-                    case (3):
-                        byte[] res3 = new byte[3] { GetHash(first, 2), GetHash(second, 4), GetHash(third, 3) };
-                        return GetHash(res3, 1);
-                    case (4):
-                        byte[] res4 = new byte[3] { GetHash(first, 1), GetHash(second, 3), GetHash(third, 4) };
-                        return GetHash(res4, 2);
-                    case (5):
-                        byte[] res5 = new byte[3] { GetHash(first, 4), GetHash(second, 2), GetHash(third, 3) };
-                        return GetHash(res5, 1);
-                    case (6):
-                        byte[] res6 = new byte[3] { GetHash(first, 5), GetHash(second, 2), GetHash(third, 1) };
-                        return GetHash(res6, 3);
-                    case (7):
-                        byte[] res7 = new byte[3] { GetHash(first, 3), GetHash(second, 4), GetHash(third, 2) };
-                        return GetHash(res7, 1);
-                    case (8):
-                        byte[] res8 = new byte[3] { GetHash(first, 3), GetHash(second, 5), GetHash(third, 4) };
-                        return GetHash(res8, 6);
-                    case (9):
-                        byte[] res9 = new byte[3] { GetHash(first, 1), GetHash(second, 5), GetHash(third, 3) };
-                        return GetHash(res9, 2);
-                    case (10):
-                        byte[] res10 = new byte[3] { GetHash(first, 6), GetHash(second, 3), GetHash(third, 1) };
-                        return GetHash(res10, 2);
-                }
+                if (gethash[i] != hash[i]) return false;
             }
-            return 0x00;
+            return true;
         }
 
         private static byte BoolToByte(bool[] input)
@@ -225,9 +151,7 @@ namespace ConsoleApp3
 
         private static string ByteToHex(byte input)
         {
-            if (input < 16)
-                return '0' + input.ToString("X");
-            return input.ToString("X");
+            return (input < 16) ? '0' + input.ToString("X") : input.ToString("X");
         }
 
         private static byte HexToByte(string input)
@@ -259,23 +183,24 @@ namespace ConsoleApp3
                 }
                 bytes[i] = BoolToByte(tmp);
             }
-            byte[] result = new byte[36];
-            result[26] = GetHash(new byte[3] { bytes[0], bytes[3], bytes[6] }, 1);
-            result[27] = GetHash(new byte[3] { bytes[9], bytes[1], bytes[4] }, 2);
-            result[28] = GetHash(new byte[3] { bytes[8], bytes[11], bytes[2] }, 3);
-            result[29] = GetHash(new byte[3] { bytes[0], bytes[9], bytes[7] }, 4);
-            result[30] = GetHash(new byte[3] { bytes[3], bytes[1], bytes[11] }, 5);
-            result[31] = GetHash(new byte[3] { bytes[6], bytes[4], bytes[2] }, 6);
-            result[32] = GetHash(new byte[2] { bytes[5], bytes[7] }, 1);
-            result[33] = GetHash(new byte[2] { bytes[10], bytes[12] }, 2);
-            result[34] = GetHash(new byte[2] { bytes[5], bytes[10] }, 3);
-            result[35] = GetHash(new byte[2] { bytes[7], bytes[12] }, 4);
+            byte[] result = new byte[34];
+            byte[] hash = GetHash(bytes);
             byte[] keys = new byte[13];
             random.NextBytes(keys);
             for (int i = 0; i < 13; i++)
             {
                 result[2 * i] = Vigenere(bytes[i], keys[i]);
                 result[2 * i + 1] = keys[i];
+                bytes[i] = (byte)(result[2 * i] ^ result[2 * i + 1]);
+            }
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result[26 + i] = hash[i];
+            }
+            hash = GetHash(bytes);
+            for (int i = 0; i < hash.Length; i++)
+            {
+                result[26 + hash.Length + i] = hash[i];
             }
             string res = "";
             for (int i = 0; i < result.Length; i++)
@@ -284,64 +209,91 @@ namespace ConsoleApp3
             }
             return res;
         }
-
-        public static void SaveArrangement(string name, bool[,]input)
+        
+        /// <summary>
+        /// Сохраняет растановку в указанный файл. Если файл существует, он будет перезаписан.
+        /// </summary>
+        /// <param name="name">Имя файла с указанным расширением.</param>
+        /// <param name="input">Расстановка кораблей.</param>
+        public static void SaveArrangement(string name, ShipArrangement input)
         {
-            string s = saveArrangement(input);
+            bool[,] arrangement = new bool[10, 10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j=0;j<10;j++)
+                {
+                    arrangement[i, j] = input.GetCellState(i, j) == CellStatе.Ship;
+                }
+            }
+            string s = saveArrangement(arrangement);
             FileStream fileStream = new FileStream(name, FileMode.Create);
             Encoding e = Encoding.ASCII;
-            fileStream.Write(e.GetBytes(s), 0, 72);
+            fileStream.Write(e.GetBytes(s), 0, e.GetBytes(s).Length);
             fileStream.Close();
         }
 
         private static bool[,] loadArrangement(string input)
         {
-            if (input.Length != 72) throw new LoadingArrangementException();
-            byte[] result = new byte[36];
-            for (int i = 0; i < 36; i++)
+            byte[] byteinput = new byte[input.Length / 2];
+            for (int i = 0; i < byteinput.Length; i++)
             {
-                result[i] = HexToByte(input[2 * i].ToString() + input[2 * i + 1]);
+                byteinput[i] = HexToByte(input[2 * i].ToString() + input[2 * i + 1]);
+            }
+            byte[] hashres = new byte[4];
+            byte[] hash = new byte[4];
+            for (int i = 0; i < hash.Length; i++)
+            {
+                hash[i] = byteinput[26 + i];
+                hashres[i] = byteinput[30 + i];
             }
             byte[] bytes = new byte[13];
             for (int i = 0; i < 13; i++)
             {
-                bytes[i] = UnVigenere(result[2 * i], result[2 * i + 1]);
+                bytes[i] = (byte)(byteinput[2 * i] ^ byteinput[2 * i + 1]);
             }
-            if (result[26] != GetHash(new byte[3] { bytes[0], bytes[3], bytes[6] }, 1)) throw new LoadingArrangementException();
-            if (result[27] != GetHash(new byte[3] { bytes[9], bytes[1], bytes[4] }, 2)) throw new LoadingArrangementException();
-            if (result[28] != GetHash(new byte[3] { bytes[8], bytes[11], bytes[2] }, 3)) throw new LoadingArrangementException();
-            if (result[29] != GetHash(new byte[3] { bytes[0], bytes[9], bytes[7] }, 4)) throw new LoadingArrangementException();
-            if (result[30] != GetHash(new byte[3] { bytes[3], bytes[1], bytes[11] }, 5)) throw new LoadingArrangementException();
-            if (result[31] != GetHash(new byte[3] { bytes[6], bytes[4], bytes[2] }, 6)) throw new LoadingArrangementException();
-            if (result[32] != GetHash(new byte[2] { bytes[5], bytes[7] }, 1)) throw new LoadingArrangementException();
-            if (result[33] != GetHash(new byte[2] { bytes[10], bytes[12] }, 2)) throw new LoadingArrangementException();
-            if (result[34] != GetHash(new byte[2] { bytes[5], bytes[10] }, 3)) throw new LoadingArrangementException();
-            if (result[35] != GetHash(new byte[2] { bytes[7], bytes[12] }, 4)) throw new LoadingArrangementException();
+            if (!CheckHash(bytes, hashres)) throw new LoadingArrangementException();
+            for (int i = 0; i < 13; i++)
+            {
+                bytes[i] = UnVigenere(byteinput[2 * i], byteinput[2 * i + 1]);
+            }
+            if (!CheckHash(bytes, hash)) throw new LoadingArrangementException();
             bool[] scrambled = new bool[104];
             for (int i = 0; i < 13; i++)
             {
-                bool[] q;
-                q = ByteToBool(bytes[i]);
-                for (int j = 0; j < 8; j++)
+                bool[] tmp = ByteToBool(bytes[i]);
+                for (int j = 0; j < tmp.Length; j++)
                 {
-                    scrambled[8 * i + j] = q[j];
+                    scrambled[i * 8 + j] = tmp[j];
                 }
             }
-            bool[] field = Unscramble(scrambled, 4);
-            bool[,] fin = Bending<bool>(field, 10, 10);
-            return fin;
+            bool[] res = Unscramble(scrambled, 4);
+            return Bending<bool>(res, 10, 10);
         }
 
-        public static bool[,] LoadArrangement(string name)
+        /// <summary>
+        /// Загружает расстановкy из указанного файла.
+        /// </summary>
+        /// <param name="name">Имя файла.</param>
+        /// <returns></returns>
+        public static ShipArrangement LoadArrangement(string name)
         {
             if (!File.Exists(name)) throw new LoadingArrangementException();
-            FileStream fileStream = new FileStream(name,FileMode.Open);
-            byte[] bytes = new byte[72];
-            fileStream.Read(bytes, 0, 72);
+            FileStream fileStream = new FileStream(name, FileMode.Open);
+            byte[] bytes = new byte[68];
+            fileStream.Read(bytes, 0, bytes.Length);
             Encoding e = Encoding.ASCII;
             string s = e.GetString(bytes);
             fileStream.Close();
-            return loadArrangement(s);
+            ShipArrangement res = new ShipArrangement();
+            bool[,] arrangement = loadArrangement(s);
+            for (int i=0;i<10;i++)
+            {
+                for (int j=0;j<10;j++)
+                {
+                    res.SetCellState((arrangement[i, j]) ? CellStatе.Ship : CellStatе.Water, i, j);
+                }
+            }
+            return res;
         }
     }
 }
