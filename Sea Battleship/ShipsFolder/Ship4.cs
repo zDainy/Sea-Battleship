b1bda40277c1d0fc1f1a877a34cdef1c709c3a4e
Александра <aksi-nait@yandex.com>
2017-12-20 00:48:01 +0400

    Что происходит

---
+++ Sea Battleship/ShipsFolder/Ship4.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Sea_Battleship
+{
+    public class Ship4 : AShip
+    {
+        public Ship4(PlayField field) //изображения задавать здесь
+        {
+           // ShipStackPanel = stackPanel;
+            playField = field;
+            IsPlaced = false;
+            IsHorizontal = true;
+            Length = 4;
+            Images = new Image[4];
+            Images[0] = new Image
+            {
+                Stretch = Stretch.Fill,
+                Source = new BitmapImage(new Uri("/Resources/1.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache },
+            };
+            Images[1] = new Image
+            {
+                Stretch = Stretch.Fill,
+                Source = new BitmapImage(new Uri("/Resources/2.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache },
+
+            };
+            Images[2] = new Image
+            {
+                Stretch = Stretch.Fill,
+                Source = new BitmapImage(new Uri("/Resources/3.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache },
+            };
+            Images[3] = new Image
+            {
+                Stretch = Stretch.Fill,
+                Source = new BitmapImage(new Uri("/Resources/4.png", UriKind.Relative)) { CreateOptions = BitmapCreateOptions.IgnoreImageCache },
+            };
+            for (int i = 0; i < Length; i++)
+            {
+                Images[i].MouseLeftButtonDown += Ship_MouseLeftButtonDown;
+            }
+        }
+
+        private void Ship_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) //в процессе
+        {
+            DeleteShipFromField();
+        }
+    }
+}
