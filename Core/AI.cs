4b07c3ce1d50d4d73bdfd1e1a4b70256c0788f28
noSerious217 <gidrolisk314@yandex.ru>
2017-12-27 22:31:48 +0400

    Чертов ИИ

--- Core/AI.cs
+++ Core/AI.cs
@@ -4,7 +4,7 @@ namespace Core
     public static class AI
     {
         private static System.Random random = new System.Random();
-        private static System.Collections.Generic.LinkedList<Vector> history = new System.Collections.Generic.LinkedList<Vector>();
+        private static System.Collections.Generic.List<Vector> Will = new System.Collections.Generic.List<Vector>();
 
         private static bool FindFourDestroyed(ShipArrangement arrangement)
         {
@@ -54,19 +54,57 @@ namespace Core
             return false;
         }
 
+        private static int Count(ShipArrangement arrangement)
+        {
+            int res = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (arrangement.GetCellState(i, j) == CellStatе.Ship || arrangement.GetCellState(i, j) == CellStatе.Water) res++;
+                }
+            }
+            return res;
+        }
+
+        private static void FillList(ShipArrangement arrangement)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (arrangement.GetCellState(i, j) == CellStatе.Ship || arrangement.GetCellState(i, j) == CellStatе.Water)
+                        Will.Add(new Vector(i, j));
+                }
+            }
+        }
+
         public static Point MakeAMove(Game g)
         {
+            if (Will.Count == 0 && Count(g.ServerShipArrangement) < 20) FillList(g.ServerShipArrangement);
             int x = 0;
             int y = 0;
             switch (g.GameConfig.BotLvl)
             {
                 case BotLevels.Easy:
-                    do
+                    if (Will.Count == 0)
                     {
-                        x = random.Next(10);
-                        y = random.Next(10);
+                        do
+                        {
+                            x = random.Next(10);
+                            y = random.Next(10);
+                        }
+                        while (g.MakeAMove(x, y) == MoveResult.Error);
                     }
-                    while (g.MakeAMove(x, y) == MoveResult.Error);
+                    else
+                    {
+                        int n = random.Next(Will.Count);
+                        Vector vector = Will[n];
+                        Will.RemoveAt(n);
+                        x = (int)vector.X;
+                        y = (int)vector.Y;
+                        g.MakeAMove(x, y);
+                    }
                     break;
                 case BotLevels.Medium:
                     int fx = -1;
@@ -92,16 +130,29 @@ namespace Core
                     }
                     if (fx == -1)
                     {
-                        do
+                        if (Will.Count == 0)
                         {
-                            x = random.Next(10);
-                            y = random.Next(10);
+                            do
+                            {
+                                x = random.Next(10);
+                                y = random.Next(10);
+                            }
+                            while (g.MakeAMove(x, y) == MoveResult.Error);
                         }
-                        while (g.MakeAMove(x, y) == MoveResult.Error);
+                        else
+                        {
+                            int n = random.Next(Will.Count);
+                            Vector vector = Will[n];
+                            Will.RemoveAt(n);
+                            x = (int)vector.X;
+                            y = (int)vector.Y;
+                            g.MakeAMove(x, y);
+                        }
+                        break;
                     }
                     else
                     {
-                        if (g.ServerShipArrangement.GetCellState(fx + 1, fy) == CellStatе.WoundedShip)
+                        if (fx + 1 < 10 && g.ServerShipArrangement.GetCellState(fx + 1, fy) == CellStatе.WoundedShip)
                         {
                             int px = fx + 1;
                             while ((g.ServerShipArrangement.GetCellState(px, fy) == CellStatе.WoundedShip) && (px < 10))
@@ -121,7 +172,7 @@ namespace Core
                                 y = fy;
                             }
                         }
-                        else if (g.ServerShipArrangement.GetCellState(fx, fy + 1) == CellStatе.WoundedShip)
+                        else if (fy + 1 < 10 && g.ServerShipArrangement.GetCellState(fx, fy + 1) == CellStatе.WoundedShip)
                         {
                             int py = fy + 1;
                             while ((g.ServerShipArrangement.GetCellState(fx, py) == CellStatе.WoundedShip) && (py < 10))
@@ -194,86 +245,140 @@ namespace Core
                     }
                     if (fx == -1)
                     {
-                        if (FindFourDestroyed(g.ServerShipArrangement))
+                        if (Will.Count == 0)
                         {
-                            if (SomethingExceptOne(g.ServerShipArrangement))
+                            if (FindFourDestroyed(g.ServerShipArrangement))
                             {
-                                int rem = 0;
-                                do
+                                if (SomethingExceptOne(g.ServerShipArrangement))
                                 {
-                                    x = random.Next(10);
-                                    y = random.Next(10);
-                                    System.Math.DivRem(x + y, 2, out rem);
+                                    int rem = 0;
+                                    do
+                                    {
+                                        x = random.Next(10);
+                                        y = random.Next(10);
+                                        System.Math.DivRem(x + y, 2, out rem);
+                                    }
+                                    while ((rem == 1) || g.MakeAMove(x, y) == MoveResult.Error);
                                 }
-                                while ((rem == 1) || g.MakeAMove(x, y) == MoveResult.Error);
+                                else
+                                {
+                                    do
+                                    {
+                                        x = random.Next(10);
+                                        y = random.Next(10);
+                                    }
+                                    while (g.MakeAMove(x, y) == MoveResult.Error);
+                                }
                             }
                             else
                             {
+                                int k = 0;
+                                int sum = 0;
                                 do
                                 {
-                                    x = random.Next(10);
-                                    y = random.Next(10);
+                                    do
+                                    {
+                                        x = random.Next(10);
+                                        double r = random.NextDouble();
+                                        sum = r < 0.077 ? 0 : r < 0.308 ? 4 : r < 0.693 ? 8 : r < 0.924 ? 12 : 16;
+                                    }
+                                    while (sum + 8 - x < 0 || sum + 8 - x >= 10);
+                                    k++;
                                 }
-                                while (g.MakeAMove(x, y) == MoveResult.Error);
+                                while (k < 20 && g.MakeAMove(x, y) == MoveResult.Error);
+                                if (k == 20)
+                                    do
+                                    {
+                                        x = random.Next(10);
+                                        y = random.Next(10);
+                                    }
+                                    while (g.MakeAMove(x, y) == MoveResult.Error);
+                                else y = sum + 8 - x;
                             }
                         }
                         else
                         {
-                            int sum = 0;
-                            do
-                            {
-                                x = random.Next(10);
-                                double r = random.NextDouble();
-                                sum = r < 0.2 ? 0 : r < 0.4 ? 4 : r < 0.6 ? 8 : r < 0.8 ? 12 : 16;
-                            }
-                            while (((sum + 8 - x >= 10) || (sum + 8 - x < 0)) || (g.MakeAMove(x, sum + 8 - x) == MoveResult.Error));
+                            int n = random.Next(Will.Count);
+                            Vector vector = Will[n];
+                            Will.RemoveAt(n);
+                            x = (int)vector.X;
+                            y = (int)vector.Y;
+                            g.MakeAMove(x, y);
                         }
+                        break;
                     }
                     else
                     {
-                        if (g.ServerShipArrangement.GetCellState(fx + 1, fy) == CellStatе.WoundedShip)
+                        if (fx + 1 < 10 && g.ServerShipArrangement.GetCellState(fx + 1, fy) == CellStatе.WoundedShip)
                         {
                             int px = fx + 1;
                             while ((g.ServerShipArrangement.GetCellState(px, fy) == CellStatе.WoundedShip) && (px < 10))
                             {
                                 px++;
                             }
-                            if ((px == 10) || (g.ServerShipArrangement.GetCellState(px, fy) == CellStatе.WoundedWater)) g.MakeAMove(fx - 1, fy);
-                            else g.MakeAMove(px, fy);
+                            if ((px == 10) || (g.ServerShipArrangement.GetCellState(px, fy) == CellStatе.WoundedWater))
+                            {
+                                g.MakeAMove(fx - 1, fy);
+                                x = fx - 1;
+                                y = fy;
+                            }
+                            else
+                            {
+                                g.MakeAMove(px, fy);
+                                x = px;
+                                y = fy;
+                            }
                         }
-                        else if (g.ServerShipArrangement.GetCellState(fx, fy + 1) == CellStatе.WoundedShip)
+                        else if (fy + 1 < 10 && g.ServerShipArrangement.GetCellState(fx, fy + 1) == CellStatе.WoundedShip)
                         {
                             int py = fy + 1;
                             while ((g.ServerShipArrangement.GetCellState(fx, py) == CellStatе.WoundedShip) && (py < 10))
                             {
                                 py++;
                             }
-                            if ((py == 10) || (g.ServerShipArrangement.GetCellState(fx, py) == CellStatе.WoundedWater)) g.MakeAMove(fx, fy - 1);
-                            else g.MakeAMove(fx, py);
+                            if ((py == 10) || (g.ServerShipArrangement.GetCellState(fx, py) == CellStatе.WoundedWater))
+                            {
+                                g.MakeAMove(fx, fy - 1);
+                                x = fx;
+                                y = fy - 1;
+                            }
+                            else
+                            {
+                                g.MakeAMove(fx, py);
+                                x = fx;
+                                y = py;
+                            }
                         }
                         else
                         {
                             if ((fx + 1 < 10) && (g.ServerShipArrangement.GetCellState(fx + 1, fy) != CellStatе.WoundedWater))
                             {
                                 g.MakeAMove(fx + 1, fy);
+                                x = fx + 1;
+                                y = fy;
                             }
                             else if ((fy + 1 < 10) && (g.ServerShipArrangement.GetCellState(fx, fy + 1) != CellStatе.WoundedWater))
                             {
                                 g.MakeAMove(fx, fy + 1);
+                                x = fx;
+                                y = fy + 1;
                             }
                             else if ((fx - 1 >= 0) && (g.ServerShipArrangement.GetCellState(fx - 1, fy) != CellStatе.WoundedWater))
                             {
                                 g.MakeAMove(fx - 1, fy);
+                                x = fx - 1;
+                                y = fy;
                             }
                             else if ((fy - 1 >= 0) && (g.ServerShipArrangement.GetCellState(fx, fy - 1) != CellStatе.WoundedWater))
                             {
                                 g.MakeAMove(fx, fy - 1);
+                                x = fx;
+                                y = fy - 1;
                             }
                         }
                     }
                     break;
             }
-            history.AddLast(new Vector(x, y));
             return new Point(x, y);
         }
     }
